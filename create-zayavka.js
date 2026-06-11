6d36f62ec1507fe0196ac128f5cc2b1191f43441
snt999999 <bebelya9@yandex.ru>
2026-06-11 10:28:53 +0500

    ю

--- create-zayavka.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest() {
-  return new Response(JSON.stringify({ ok: true, message: "Cloudflare Pages Functions работают", version: "solncanet-site-v7" }, null, 2), {
-    headers: { "Content-Type": "application/json; charset=utf-8" }
-  });
-}
